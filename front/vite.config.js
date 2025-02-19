47ac7f639b1b5b7201e6a335554853fdb3451976
ikerlorente11 <ikerlorentecalvo@gmail.com>
2025-02-19 13:32:21 +0100

    Update vite config

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 
+const isDemo = process.env.ENV === 'demo';
+const baseUrl = process.env.PUBLIC_BASE_URL || '';
+
 export default defineConfig({
     server: {
         host: true,
         port: 4321, // Debe coincidir con el de Astro y Docker
         strictPort: true,
         allowedHosts: ["serveriker.ddns.net"], // Permitir este host específico
-    }
+    },
+    base: isDemo ? `${baseUrl}/` : '/',
 });
